8e7a14e430875cd67eb5713c3e944f405ba9ff20
Hotaru24 <florent-pasquet@protonmail.com>
2021-03-24 23:35:30 +0100

    pie graph is created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
+import { ChartsModule } from 'ng2-charts'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { ListComponent } from './list/list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pie/pie.component.ts
+++ src/app/pie/pie.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CitiesListService } from '../services/cities-list.service';
+
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-pie',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PieComponent implements OnInit {
 
-  constructor() { }
+  constructor(public ct: CitiesListService) { }
+  
+  citiesCopy : any[] = [];
 
   ngOnInit(): void {
+    this.getCities();
+    let myChart = new Chart("myChart", {
+      type: 'pie',
+      data: {
+          labels: [
+            this.citiesCopy[0].city, 
+            this.citiesCopy[1].city, 
+            this.citiesCopy[2].city, 
+            this.citiesCopy[3].city, 
+            this.citiesCopy[4].city
+          ],
+          datasets: [{
+              label: '# of Votes',
+              data: [37435191, 29399141, 26317104, 21846507, 21671908],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)'
+              ],
+              borderWidth: 1
+          }]
+      }
+  });
   }
 
+  getCities(){
+    this.citiesCopy = this.ct.getCities();
+  }
 }
