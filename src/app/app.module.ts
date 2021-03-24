154e77dcc4853186412a73eb639286cf58068ceb
Hotaru24 <florent-pasquet@protonmail.com>
2021-03-24 10:34:32 +0100

    components are created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { PieComponent } from './pie/pie.component';
+import { LineComponent } from './line/line.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    PieComponent,
+    LineComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
