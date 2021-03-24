cab7de4c4f5ac8ad7c7c03fe8872c8edf0b95f7a
Hotaru24 <florent-pasquet@protonmail.com>
2021-03-24 10:47:07 +0100

    routing is created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LineComponent } from './line/line.component';
+import { ListComponent } from './list/list.component';
+import { MapComponent } from './map/map.component';
+import { PieComponent } from './pie/pie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MapComponent},
+  {path: 'pie', component: PieComponent},
+  {path: 'line', component: LineComponent},
+  {path: 'list', component: ListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
